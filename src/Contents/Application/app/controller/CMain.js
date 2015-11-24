ced5e706fb9ad303b5d30a65865c6305edfdcc78
XXX <xxx@xxx.com>
2015-11-24 10:18:59 +0100

    Update# 46

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -16,6 +16,9 @@ App.controller.define('CMain', {
 		this.control({
 			"menu>menuitem": {
 				click: "Menu_onClick"
+			},
+			"mainform": {
+				show: "onShow"
 			}
 		});
 		
@@ -28,35 +31,34 @@ App.controller.define('CMain', {
 			Ext.Msg.alert('Status', 'Click event on '+p.itemId);
 		};			
 	},
+	onAuth: function(p,user) {
+		EVT_CURRENT.user = user.mail;
+		if (user.profiles.indexOf('ADMIN')>-1) Ext.getCmp('MNU_ADMIN').setVisible(true);
+		App.get('Menu#MenuPanel').update();
+		var o = {
+			Mail: EVT_CURRENT.user,
+		};
+		App.get("mainform combo#selectAgent").setValue(0);
+		/*App.reservation.getInfo(o, function(err, result) {	
+			numLogin = result.result.data[0].Id;
+			EVT_CURRENT.numLogin = numLogin;
+			App.get('schedulergrid#schedule').getEventStore().load();
+		});*/
+
+		var now = new Date();
+		App.get('schedulergrid#schedule').getResourceStore().getProxy().extraParams._cfg = 0;
+		App.get('schedulergrid#schedule').getResourceStore().load();	
+	},
+	onShow: function(p)
+	{
+		var me=this;
+		Auth.login(function(user) {
+			me.onAuth(p, user);
+		});	
+	},
 	onLoad: function(p)
 	{
-		return;
-		function onAuth(p,user) {
-            EVT_CURRENT.user = user.mail;
-			if (user.profiles.indexOf('ADMIN')>-1) Ext.getCmp('MNU_ADMIN').setVisible(true);
-			App.get('Menu#MenuPanel').update();
-            var o = {
-                Mail: EVT_CURRENT.user,
-            };
-			App.get("mainform combo#selectAgent").setValue(0);
-            /*App.reservation.getInfo(o, function(err, result) {	
-                numLogin = result.result.data[0].Id;
-                EVT_CURRENT.numLogin = numLogin;
-                App.get('schedulergrid#schedule').getEventStore().load();
-            });*/
-
-            var now = new Date();
-            App.get('schedulergrid#schedule').getResourceStore().getProxy().extraParams._cfg = 0;
-            App.get('schedulergrid#schedule').getResourceStore().load();
-			App.get('schedulergrid#schedule').getResourceStore().on('load',function() {
-				alert('bravo');
-			});
-		
-		};
-		
-		Auth.login(function(user) {
-			onAuth(p, user);
-		});
+				
 	}
 	
 	
