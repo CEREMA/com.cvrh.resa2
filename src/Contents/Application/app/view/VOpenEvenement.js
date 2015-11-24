43b4b7415cefbf80cdc57babb620f6d17f374b9f
XXX <xxx@xxx.com>
2015-11-24 16:21:44 +0100

    Update# 124

---
+++ src/Contents/Application/app/view/VOpenEvenement.js
@@ -0,0 +1,82 @@
+App.view.define('VOpenEvenement', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.TOpenEvenement',
+
+    initComponent: function() {
+        this.width = 640;
+        this.height = 390;
+		this.closable=false;
+        this.title = "Choississez un évènement";
+        this.bodyCls = "white";
+		this.layout="fit";
+        this.items = [{
+                xtype: "treepanel",
+                itemId: "tree1",
+                useArrows: true,
+                rootVisible: false,
+                height: "100%",
+                store: App.treestore.create('TreeModel', {
+                    root: {
+                        expanded: true, 
+						children: [],
+						autoLoad: false
+                    }
+                }),
+                multiSelect: false,
+                singleExpand: false,
+                columns: [{
+                xtype: 'treecolumn', //this is so we know which column will show the tree
+                text: 'Evènement',
+				width: 250,
+                sortable: true,
+                dataIndex: 'text'
+				},
+				{
+                text: 'Typologie',
+				width: 130,
+                sortable: true,
+                dataIndex: 'typo'
+				},
+				{
+              
+                text: 'Statut',
+				width: 60,
+                //flex: 1,
+				align: 'center',
+                sortable: true,
+                dataIndex: 'statutResa',
+				renderer : function(val){
+					return '<span style="background-color:#'+val+';">&nbsp;&nbsp;&nbsp;&nbsp;</span>';
+				},
+            },
+			
+			{
+              
+                text: 'Début formation',
+				width: 90,
+                //flex: 1,
+				align: 'center',
+                sortable: true,
+                dataIndex: 'debutModule'
+            },
+			{
+              
+                text: 'Fin formation',
+                flex: 1,
+				align: 'center',
+                sortable: true,
+                dataIndex: 'finModule'
+            },
+			
+			
+			] 
+            }],
+			 this.bbar = ['->',{
+                    xtype: "button",
+                    text: "Quitter",
+                    itemId: "CANCEL_Open_evenement"
+                },
+            ],
+            this.callParent(arguments);
+    }
+});
