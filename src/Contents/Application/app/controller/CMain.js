71e637b68d02685a7972b5016a0d39cc14ddbcbf
XXX <xxx@xxx.com>
2015-11-24 11:22:06 +0100

    Update# 57

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -51,40 +51,13 @@ App.controller.define('CMain', {
 		var mm = ((now.getMonth() + 1) >= 10) ? (now.getMonth() + 1) : '0' + (now.getMonth() + 1);
 		
 		App.get('combo#selectMonth').setValue(parseInt(mm)-1);
-		
-		/*if (mm == "01") {
-			App.get('combo#selectMonth').setValue(0)
-		} else if (mm == "02") {
-			App.get('combo#selectMonth').setValue(1)
-		} else if (mm == "03") {
-			App.get('combo#selectMonth').setValue(2)
-		} else if (mm == "04") {
-			App.get('combo#selectMonth').setValue(3)
-		} else if (mm == "05") {
-			App.get('combo#selectMonth').setValue(4)
-		} else if (mm == "06") {
-			App.get('combo#selectMonth').setValue(5)
-		} else if (mm == "07") {
-			App.get('combo#selectMonth').setValue(6)
-		} else if (mm == "08") {
-			App.get('combo#selectMonth').setValue(7)
-		} else if (mm == "09") {
-			App.get('combo#selectMonth').setValue(8)
-		} else if (mm == "10") {
-			App.get('combo#selectMonth').setValue(9)
-		} else if (mm == "11") {
-			App.get('combo#selectMonth').setValue(10)
-		} else if (mm == "12") {
-			App.get('combo#selectMonth').setValue(11)
-		}*/
 
 		function isWeekend(d) {
 			return (d.getDay() == 6);
 		};
-		//App.get('schedulergrid#schedule').disable();					
+
 		var year = now.getFullYear();
 		var month = App.get('combo#selectMonth').getValue();
-
 
 		function days_in_month(month, year) {
 			var m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -94,9 +67,8 @@ App.controller.define('CMain', {
 			return m[1] + 1; //tous les autres févriers = 29 jours
 		}
 
-
-		days_in_month(mm, 2015);
-		var resultat = days_in_month(mm, 2015) + 1;
+		days_in_month(mm, new Date().getFullYear());
+		var resultat = days_in_month(mm, new Date().getFullYear()) + 1;
 		EVT_CURRENT.resultat = resultat - 1;
 
 		App.get('schedulergrid#schedule').setStart(new Date(year, month, 1));
