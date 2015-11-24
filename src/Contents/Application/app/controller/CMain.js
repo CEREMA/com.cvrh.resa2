3ecc9529f56a99fe721d5e5daeb9e67f1fb6a241
XXX <xxx@xxx.com>
2015-11-24 09:53:38 +0100

    Update# 32

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -28,7 +28,29 @@ App.controller.define('CMain', {
 	},
 	onLoad: function()
 	{
+		function onAuth(p,user) {
+            EVT_CURRENT.user = user.mail;
+			if (user.profiles.indexOf('ADMIN')>-1) Ext.getCmp('MNU_ADMIN').setVisible(true);
+			App.get('Menu#MenuPanel').update();
+            var o = {
+                Mail: EVT_CURRENT.user,
+            };
+			App.get("mainform combo#selectAgent").setValue(0);
+            App.reservation.getInfo(o, function(err, result) {	
+                numLogin = result.result.data[0].Id;
+                EVT_CURRENT.numLogin = numLogin;
+                App.get('schedulergrid#schedule').getEventStore().load();
+            });
+
+            var now = new Date();
+            App.get('schedulergrid#schedule').getResourceStore().getProxy().extraParams._cfg = 0;
+            App.get('schedulergrid#schedule').getResourceStore().load();
 		
+		};
+		
+		Auth.login(function(user) {
+			onAuth(p, user);
+		});
 	}
 	
 	
