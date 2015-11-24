85a9ca8e365a5fb927a8fd897810387e8ca778e7
XXX <xxx@xxx.com>
2015-11-24 16:03:02 +0100

    Update# 115

--- src/Contents/Culture/fr.js
+++ src/Contents/Culture/fr.js
@@ -1,3 +1,5 @@
 i18n['fr'] = {	
-	region  			: 	"Bonjour le monde !"
+	region  			: 	"Bonjour le monde !",
+	PASSPORT_GOOGLE		: 	"Connectez vous via Google",
+	PASSPORT_CERBERE		: 	"Connectez vous via Cerbère"
 };
