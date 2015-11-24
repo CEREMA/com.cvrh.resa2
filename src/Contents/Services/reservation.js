28a5909e5f2c95ca598b4bf9a48f177b45ca7fbe
XXX <xxx@xxx.com>
2015-11-24 10:30:40 +0100

    Update# 54

--- src/Contents/Services/reservation.js
+++ src/Contents/Services/reservation.js
@@ -8,3 +8,5 @@ reservation={
         reservation.using('db').model('reservation_salles', 'select Id,profil,LOWER(mail) as mail from agents where mail="' + o.Mail + '"', cb);	
 	}
 };
+
+module.exports=reservation;
