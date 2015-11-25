74a60cd81127fb32f67d58d1f944bddf284c9ca4
Stéphane Zucatti <stephane.zucati@me.com>
2015-11-25 10:50:52 +0100

    Update# 169

---
+++ src/Contents/Application/app/view/admin/VAdmin.js
@@ -0,0 +1,58 @@
+App.view.define('VAdmin', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.TAdmin',
+
+    initComponent: function() {
+        this.width = 640;
+        this.height = 390;
+		this.closable=true;
+        this.title = "Administration";
+        this.bodyCls = "white";
+		this.layout="vbox";
+        this.items = [
+			{
+				xtype: "grid",
+				itemId: "dbgrid",
+				width: "100%",
+				flex: 1,
+				store: App.store.create({
+					fields: [],
+					data: []
+				}),
+				columns: [
+				
+				],
+				plugins: [
+					"bufferedrenderer",
+					Ext.create('Ext.ux.grid.plugin.RowEditing', {
+						clicksToMoveEditor: 1,
+						autoCancel: false
+					})
+				]
+			}
+		];
+		this.tbar = [
+		{
+			text: 'Ajouter',
+			itemId: 'add',
+			iconCls: 'add'
+		}, 
+		{
+			itemId: 'remove',
+			text: 'Supprimer',
+			iconCls: 'remove',
+			disabled: true
+		},
+		'->',
+		{
+				xtype: "combo",
+				fieldLabel: "Table",
+				itemId: "combo_db",
+				store: App.store.create('reservation_salles://*'),
+				displayField: "TABLE_NAME",
+				valueField: "TABLE_NAME"
+        }			
+        ];
+        this.callParent(arguments);
+    }
+});
---
+++ src/Contents/Application/app/view/admin/VAdminJF.js
@@ -0,0 +1,95 @@
+App.view.define('VAdminJF', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.TAdminJF',
+
+    initComponent: function() {
+        this.width = 640;
+        this.height = 390;
+		this.closable=true;
+        this.title = "Jours fériés";
+        this.bodyCls = "white";
+		this.layout="vbox";
+        this.items = [
+			{
+				layout: "hbox",
+				width: "100%",
+				border: false,
+				items: [
+				{
+					xtype: "textfield",
+					itemId: "id",
+					hidden: true
+				},
+				{
+					xtype: "datefield",
+					itemId: "dd"
+				},
+				{
+					xtype: "datefield",
+					itemId: "df"
+				},
+				{
+					xtype: "textfield",
+					itemId: "type",
+					flex: 1
+				},
+				{
+					xtype: "button",
+					text: "Nouveau",
+					width: 100,
+					height: "100%",
+					itemId: "new_jf"
+				},
+				{
+					xtype: "button",
+					width: 20,
+					height: "100%",
+					cls: "checkmark",
+					itemId: "del_jf"
+				},
+				{
+					xtype: "button",
+					width: 20,
+					height: "100%",
+					cls: "trashmark",
+					itemId: "rec_jf"
+				}
+				]
+			},
+			{
+				xtype: "grid",
+				itemId: "jf",
+				width: "100%",
+				flex: 1,
+				store: App.store.create("reservation_salles://off",{autoLoad: true}),
+				columns: [
+				{
+					text: "Début",
+					dataIndex: "StartDate",
+					format: 'd/m/Y',
+					renderer:Ext.util.Format.dateRenderer('d/m/Y')
+				},
+				{
+					text: "Fin",
+					dataIndex: "EndDate",
+					format: 'd/m/Y',
+					renderer:Ext.util.Format.dateRenderer('d/m/Y')
+				},
+				{
+					text: "Motif",
+					flex: 1,
+					dataIndex: "Type"
+				}				
+				]
+			}
+		];
+		this.bbar = [
+			'->',
+			{
+				text: "Fermer",
+				itemId: "close_jf"
+            }
+        ];
+        this.callParent(arguments);
+    }
+});
