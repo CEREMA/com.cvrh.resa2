7bdccb4fa7a7564895f86dfa9a6690535f920c53
Stéphane Zucatti <stephane.zucati@me.com>
2015-11-25 11:28:26 +0100

    Update# 202

---
+++ src/Contents/Application/app/controller/admin/CBackoffice.js
@@ -0,0 +1,20 @@
+App.controller.define('admin.CBackoffice', {
+
+	views: [
+		"admin.VBackoffice"
+	],
+	
+	models: [
+		
+	],
+	
+	init: function()
+	{
+
+		this.control({
+
+		});
+				
+	}
+		
+});
