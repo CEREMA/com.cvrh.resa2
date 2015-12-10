0526b7a178f8a9b119926a133aff8531170df74c
Stéphane Zucatti <stephane.zucati@me.com>
2015-12-10 11:28:27 +0100

    Update# 264

--- src/Contents/Application/app/view/VCreateEvenement.js
+++ src/Contents/Application/app/view/VCreateEvenement.js
@@ -27,6 +27,7 @@ App.view.define('VCreateEvenement', {
             layout: "form",
             margin: 15,
 			width: "100%",
+			anchor: '100% 100%',
 			flex: 1,
             border: false,
             items: [
