5c09d969e03be0ccf626ec17a04d8f6fb5f98bb1
Stéphane Zucatti <stephane.zucati@me.com>
2015-12-10 11:31:08 +0100

    Update# 267

--- src/Contents/Application/app/view/VCreateEvenement.js
+++ src/Contents/Application/app/view/VCreateEvenement.js
@@ -1,12 +1,3 @@
-Ext.override(Ext.form.FormPanel, {
-	onRender : function(ct, position){
-
-        this.initFields();
-        Ext.FormPanel.superclass.onRender.call(this, ct, position);
-		this.form.initEl(this.body);
-    }
-	
-    });
 App.view.define('VCreateEvenement', {
     extend: "Ext.window.Window",
     alias: 'widget.VCreateEvenement',
