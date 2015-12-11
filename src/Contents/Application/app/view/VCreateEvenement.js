f89836d67a5c21657d88aa1b6aba01ada0a1aefa
Stéphane Zucatti <stephane.zucati@me.com>
2015-12-11 15:41:44 +0100

    Update# 377

--- src/Contents/Application/app/view/VCreateEvenement.js
+++ src/Contents/Application/app/view/VCreateEvenement.js
@@ -240,7 +240,11 @@ App.view.define('VCreateEvenement', {
 						{
 							header: "Site",
 							width: 100,
-							dataIndex: "id_site",
+							dataIndex: "nomsalle"/*,
+							renderer : function (value) {
+								var record = comboStore.findRecord('id', value);
+								return record.title;
+							}*/,
 							editor: {
 								xtype: 'combobox',
 								typeAhead: true,
