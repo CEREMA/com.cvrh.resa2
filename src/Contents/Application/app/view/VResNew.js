5fe59017a74d20474e3be92607c99c03eb3ad354
Stéphane Zucatti <stephane.zucati@me.com>
2015-12-15 13:41:08 +0100

    Update# 350

--- src/Contents/Application/app/view/VResNew.js
+++ src/Contents/Application/app/view/VResNew.js
@@ -20,12 +20,12 @@ App.view.define('VResNew', {
 				var grid=p.up('window').grid;
 				var dta=App.getData(p.up('window'));
 				console.log(dta);
-				if (dta.p0=="J") dta.d0=dta.d0.setHours(8);
-				if (dta.p0=="M") dta.d0=dta.d0.setHours(8);
-				if (dta.p0=="A") dta.d0=dta.d0.setHours(14);
-				if (dta.p1=="J") dta.d1=dta.d1.setHours(8);
-				if (dta.p1=="M") dta.d1=dta.d1.setHours(8);
-				if (dta.p1=="A") dta.d1=dta.d1.setHours(14);
+				if (dta.p0=="J") dta.d0.setHours(8);
+				if (dta.p0=="M") dta.d0.setHours(8);
+				if (dta.p0=="A") dta.d0.setHours(14);
+				if (dta.p1=="J") dta.d1.setHours(8);
+				if (dta.p1=="M") dta.d1.setHours(8);
+				if (dta.p1=="A") dta.d1.setHours(14);
 				var data=[
 				{
 					id_salle: dta.id_salle,
