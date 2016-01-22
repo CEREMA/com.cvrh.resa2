cda91cfa58058690b4330dd8906aeda315b8bead
Stéphane Zucatti <stephane.zucatti@me.com>
2016-01-22 14:33:24 +0100

    Update# 753

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -279,7 +279,7 @@ App.view.define('VMain', {
                 sortable: true,
                 field: new Ext.form.TextField()
             }],            
-            eventStore: App.eventstore.create("reservation_salles://ressourcesalles{id_ressource=Id,id_salle=ResourceId,debutRessource=StartDate,finRessource=EndDate,'000'=User,'yellow'=Cls}",{autoLoad: true}),
+            eventStore: App.eventstore.create("reservation_salles://ressourcesalles{id_ressource=Id,id_salle=ResourceId,debutRessource=StartDate,finRessource=EndDate,'000'=User,'yellow'=Cls}"),
             resourceStore: App.resourcestore.create("App.resources.getAll",{autLoad: true})
         }]
     }]
