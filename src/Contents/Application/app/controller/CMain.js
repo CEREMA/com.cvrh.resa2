9c16b92aa720eed1ac218a4f542007e4e6cb7f79
Stéphane Zucatti <stephane.zucatti@me.com>
2016-01-25 14:45:50 +0100

    Update# 796

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -247,6 +247,8 @@ App.controller.define('CMain', {
                     App.get(p,'combo#cboSession').getStore().loadData(data);
                     App.get(p,'combo#cboSession').getStore().load();
                     App.get(p,'combo#cboSession').setValue(p.session);
+                    App.get(p,'combo#cboCP').setValue(p.chefProjet);
+                    App.get(p,'combo#cboAssistant').setValue(p.assistant);
                 });
             });
         } else {
