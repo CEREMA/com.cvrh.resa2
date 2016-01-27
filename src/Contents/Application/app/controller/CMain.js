6874456ee10a15bf906492e0d1713762be8d521e
Stéphane Zucatti <stephane.zucatti@me.com>
2016-01-27 16:03:15 +0100

    Update# 892

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -258,17 +258,15 @@ App.controller.define('CMain', {
             statutResa: "FFFF00"
         };
         if (panel.moduleID) obj.id_module=panel.moduleID;
-        App.DB.post('reservation_salles://module',obj,function(rr) {
-            var data=App.get(panel,'grid').getStore().data.items;
-            if (!rr.insertId) var x=panel.moduleID; else x=rr.insertId;
-            if (x.length>0) {
-                me.updateResources(data,x,0,function() {
-                    if (ndx+1<panels.length) me.updateModules(panels,r,ndx+1,cb); else cb();
-                });
-            } else {
+        var data=App.get(panel,'grid').getStore().data.items;
+        var x=panel.moduleID;
+        if (data.length>0) {
+            me.updateResources(data,x,0,function() {
                 if (ndx+1<panels.length) me.updateModules(panels,r,ndx+1,cb); else cb();
-            }
-        });
+            });
+        } else {
+            if (ndx+1<panels.length) me.updateModules(panels,r,ndx+1,cb); else cb();
+        }
     },
     insert_evenement: function(p)
     {
