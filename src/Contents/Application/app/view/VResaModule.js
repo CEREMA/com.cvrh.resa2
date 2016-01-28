b5b05ab96d65971d4e359b57f41f1ed8f77c8c9f
Stéphane Zucatti <stephane.zucatti@me.com>
2016-01-28 18:17:56 +0100

    Update# 981

--- src/Contents/Application/app/view/VResaModule.js
+++ src/Contents/Application/app/view/VResaModule.js
@@ -13,32 +13,7 @@ App.view.define('VResaModule', {
 		this.width = "100%";
         
         this.closable = true;
-        
-        this.listeners={
-            beforeclose: function(panel){
-                var me=this;
-                var panels=App.get('VCreateEvenement tabpanel').items.items;
-                var activeTabIndex = App.get('VCreateEvenement tabpanel').items.findIndex('id', panel.id)+1;
-                if (activeTabIndex==panels.length) {
-                    // on delete toutes les ressources associées au module
-                    App.DB.get('reservation_salles://ressourcesalles{id_ressource}?id_module='+panel.moduleID,function(e,r) {
-                        var res=[];
-                        for (var i=0;i<r.result.data.length;i++) res.push(r.result.data[i].id_ressource);
-                        App.DB.del('reservation_salles://ressourcesalles',res,function(e,r) {
-                            App.DB.del('reservation_salles://module?id_module='+panel.moduleID,function(e,r) {
-                                // on met à jour la session
-                                me.updateSession(panel.up('window'));
-                            });
-                        });
-                    });
-                    return false;
-                } else {
-                    alert('Vous ne pouvez supprimer que le dernier module !');
-                    return false;
-                }                
-            }
-        };
-        
+                
         this.items = [
 				{
 					layout: "hbox",
