08b951f084d67d798192b8674169f94833e790c6
Stéphane Zucatti <stephane.zucati@me.com>
2016-01-30 20:15:11 +0100

    Update# 1110

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -335,12 +335,12 @@ App.controller.define('CMain', {
     {
         var me=this;
         p.up('window').session=p.getValue();
-        App.get('VCreateEvenement combo#cboCP').setValue('');
-        App.get('VCreateEvenement combo#cboAssistant').setValue('');
-        App.get('VCreateEvenement numberfield#participant').setValue('');
         
         // On enregistre la session courante avant
         this.insert_evenement(App.get("VCreateEvenement button#insert_evenement"),function() {
+            App.get('VCreateEvenement combo#cboCP').setValue('');
+            App.get('VCreateEvenement combo#cboAssistant').setValue('');
+            App.get('VCreateEvenement numberfield#participant').setValue('');
             me.updateSession(p.up('window'));    
         });
     },
