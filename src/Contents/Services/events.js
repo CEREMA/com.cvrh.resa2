4f5fd58da168c8a37c88dc5755258e2a5fa0e307
Stéphane Zucatti <stephane.zucati@me.com>
2016-01-31 12:18:36 +0100

    Update# 1154

--- src/Contents/Services/events.js
+++ src/Contents/Services/events.js
@@ -5,7 +5,7 @@ Events
 events = {
     get: function(o,cb) {
         var db = events.using('db');
-        var sql='SELECT * FROM evenement WHERE id_evenement in (SELECT id_evenement FROM session WHERE chefProjet='+o.uid+' or assistant='+o.uid+')';
+        var sql='SELECT * FROM evenement WHERE id_evenement in (SELECT id_evenement FROM session WHERE status="I" and (chefProjet='+o.uid+' or assistant='+o.uid+')';
         db.model('reservation_salles',sql,cb);
     },
 	getAll: function(o,cb) {
