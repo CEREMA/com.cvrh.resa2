fb1ca5c99ebad0538c3f836e5cb879618031ff2c
Stéphane Zucatti <stephane.zucatti@me.com>
2016-02-15 10:01:47 +0100

    Update# 1199

--- src/Contents/Application/app/controller/admin/CDayOff.js
+++ src/Contents/Application/app/controller/admin/CDayOff.js
@@ -40,6 +40,13 @@ App.controller.define('admin.CDayOff', {
 		App.get('TAdminJF textfield#type').setValue(store.data.Type);
 	},
 	do_rec_jf: function(p) {
+		function days_in_month(month, year) {
+			var m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+			if (month != 2) return m[month - 1]; //tout sauf février
+			if (year % 4 != 0) return m[1]; //février normal non bissextile
+			if (year % 100 == 0 && year % 400 != 0) return m[1]; //février bissextile siècle non divisible par 400
+			return m[1] + 1; //tous les autres févriers = 29 jours
+		};        
 		var obj={
 			StartDate: App.get('TAdminJF datefield#dd').getValue(),
 			EndDate: App.get('TAdminJF datefield#df').getValue(),
@@ -77,6 +84,13 @@ App.controller.define('admin.CDayOff', {
 		});
 	},
 	do_del_jf: function(p) {
+		function days_in_month(month, year) {
+			var m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+			if (month != 2) return m[month - 1]; //tout sauf février
+			if (year % 4 != 0) return m[1]; //février normal non bissextile
+			if (year % 100 == 0 && year % 400 != 0) return m[1]; //février bissextile siècle non divisible par 400
+			return m[1] + 1; //tous les autres févriers = 29 jours
+		};        
 		var grid=App.get('TAdminJF grid#jf');
 		var selected=grid.getSelectionModel().selected.items;
 		Ext.MessageBox.confirm('Resa', 'Voulez vous vraiment supprimer cet enregistrement ?', function(btn){
@@ -89,6 +103,26 @@ App.controller.define('admin.CDayOff', {
 					App.get('TAdminJF datefield#dd').setValue("");
 					App.get('TAdminJF datefield#df').setValue("");
 					App.get('TAdminJF textfield#type').setValue("");					
+                    App.DB.get('reservation_salles://off', function(p,r) {
+                        // add weekends to off day
+                        var weekends = [];
+                        for (var i=0;i<r.result.data.length;i++) {
+                            r.result.data[i].StartDate=r.result.data[i].StartDate.toDate();
+                            r.result.data[i].EndDate=r.result.data[i].EndDate.toDate();
+                        };
+                        var resultat = days_in_month(mm, year) + 1;
+                        for (var i = 1; i < resultat; i++) {
+                            var d = new Date(year, month, i);
+                            if (isWeekend(d)) r.result.data.push({
+                                StartDate: new Date(year, month, i),
+                                EndDate: new Date(year, month, i + 2),
+                                Type: "Week-end"
+                            });
+                        };
+                        var scheduler=App.get('mainform schedulergrid#schedule');
+                        scheduler.plugins[0].store.loadData(r.result.data);
+                        scheduler.getEventStore().load();
+                    });
 				});
 			} else {
 				App.notify('Action annulée');
