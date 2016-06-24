e8e008a2336f1111fae0fab0a211393040671ce3
Stéphane Zucatti <stephane.zucati@me.com>
2016-06-24 15:01:01 +0200

    Update# 1369

--- src/Contents/Services/reservation.js
+++ src/Contents/Services/reservation.js
@@ -11,8 +11,7 @@ reservation={
 	{
 		o.DebutRessource=o.DebutRessource.split('T')[0]+' 08:00:00';
 		o.FinRessource=o.FinRessource.split('T')[0]+' 18:00:00';
-		console.log("select id_salle, nomSalle from salle where id_site=1 and salle.id_salle not in (select id_salle FROM ressourcesalles WHERE status<>'D' and '"+o.DebutRessource+"' <= finRessource AND '"+o.FinRessource+"' >= debutRessource)");
-        reservation.using('db').model('resalia',"select id_salle, nomSalle from salle where id_site="+o.id_site+" and salle.id_salle not in (select id_salle FROM ressourcesalles WHERE status<>'D' and '"+o.DebutRessource+"' <= finRessource AND '"+o.FinRessource+"' >= debutRessource)", cb);
+        reservation.using('db').model('resalia',"select id_salle, nomSalle from salle where id_site="+o.id_site+" and salle.id_salle not in (select id_salle FROM ressourcesalles WHERE status<>'D' and '"+o.DebutRessource+"' <= finRessource AND '"+o.FinRessource+"' >= debutRessource) order by nomSalle", cb);
 	},
 	getAllFormations: function(o,cb) {
 
