53ac22852cb373ab5e3c7cc2cc511534d03e475d
Stéphane Zucatti <stephane.zucati@me.com>
2016-06-24 15:11:45 +0200

    Update# 1372

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -801,10 +801,10 @@ App.controller.define('CMain', {
         if (dta.p1=="M") dta.d1.setHours(12);
         if (dta.p1=="A") dta.d1.setHours(18);
         
-        alert(App.get(p.up('window'),'checkbox#check_valider').getValue());
-        if (App.get(p.up('window'),'checkbox#check_valider').getValue()==1) dta.afficher=1;
-        if (App.get(p.up('window'),'checkbox#check_afficher').getValue()==1) dta.valider=1;
-        if (App.get(p.up('window'),'checkbox#check_preparation').getValue()==1) dta.preparation=1;
+        
+        dta.afficher=App.get(p.up('window'),'checkbox#check_valider').getValue();
+        dta.valider=App.get(p.up('window'),'checkbox#check_afficher').getValue();
+        dta.preparation=App.get(p.up('window'),'checkbox#check_preparation').getValue();
         
         var data=[
         {
