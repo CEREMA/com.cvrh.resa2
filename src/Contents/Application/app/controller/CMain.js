5a8248c9f15b0e933fb115adb71ec0b49991fe19
Stéphane Zucatti <stephane.zucati@me.com>
2016-06-28 10:16:41 +0200

    Update# 1441

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -876,7 +876,7 @@ App.controller.define('CMain', {
         };  
         
         console.log(obj);
-        
+        return;
         App.DB.post('resalia://ressourcesalles',obj,function(e) {            
             if (old_obj) data[0].id_res=old_obj.id_res; else data[0].id_res=e.insertId;
             grid.getStore().add(data);    
