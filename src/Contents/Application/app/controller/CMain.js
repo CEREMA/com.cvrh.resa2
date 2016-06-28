3a39e023a1cb36c6d6e7d14398e207b2696e35c2
Stéphane Zucatti <stephane.zucati@me.com>
2016-06-28 21:57:14 +0200

    Update# 1476

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -869,6 +869,7 @@ App.controller.define('CMain', {
         
         if (App.get('VCreateEvenement combo#cboTypologie').getValue()==4) obj.clsRessource="red";
         if (App.get('VCreateEvenement combo#cboTypologie').getValue()!=4) {
+			alert('xxx');
             obj.clsRessource="orange";
             if (dta.afficher) obj.clsRessource="green";
         };
